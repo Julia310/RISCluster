95ffe0a2089ad370d8b29c4b032f2754b28c7bb7
Julia310 <JPeters92@protonmail.com>
2023-12-13 14:19:04 +0100

    find initial centroids for clustering

---
+++ GMM_centroids.py
@@ -0,0 +1,75 @@
+from RISCluster.production import train
+from RISCluster import utils
+import os
+
+# Main project folder to save outputs:
+project_folder = '.'
+# Path to configuration files:
+path_config = f"{project_folder}/Config"
+# Path to folder containing data, including HDF file for ML workflow:
+path_data = f"{project_folder}/Data"
+# Path to raw seismic data:
+# path_data_seismo = f"{path_data}/Seismo"
+path_data_seismo = "./zdata2/data/wfjenkin/RIS_Seismic"
+# Path to save workflow outputs (ML models, figures, results, etc.)
+path_output = f"{project_folder}/Outputs"
+# Path to HDF dataset:
+fname_dataset = f"{path_data}/RISData_20210713.h5"
+# Path to save paper-ready figures:
+figure_savepath = f"{path_output}/Figures"
+
+os.makedirs('./Config/', exist_ok=True)
+exp_name = "FullArray"
+
+# Image Sample Indexes for Example Waveforms:
+img_index = [1, 2, 3, 4]
+
+# Generate new sample index for data set?
+genflag = False
+
+
+universal = {
+    'exp_name': exp_name,
+    'fname_dataset': fname_dataset,
+    'savepath': path_output,
+    'indexpath': os.path.join(path_data, 'TraValIndex_M=50000.pkl'),
+    'configpath': path_config
+}
+device_no = 0
+#device = utils.set_device(device_no)
+transform = 'sample_norm_cent'
+
+
+batch_size = 16
+LR = 0.001
+
+expserial = 'Exp20231212T174200'
+runserial = f'Run_BatchSz={batch_size}_LR={LR}'
+# exp_path = f"{path_output}/Models/AEC/{expserial}/{runserial}"
+exp_path_AEC = os.path.join(path_output, 'Models', 'AEC', expserial, runserial)
+
+weights_AEC = os.path.join(exp_path_AEC, 'AEC_Params_Final.pt')
+
+
+parameters = {
+    'model': 'GMM',
+    'mode': 'fit',
+    'show': False,
+    'send_message': False,
+    'transform': transform,
+    'img_index': str(img_index)[1:-1],
+    'tb': False,
+    'workers': 8,
+    'loadmode': 'ram',
+    'datafiletype': 'h5',
+    'saved_weights': weights_AEC
+}
+hyperparameters = {
+    'n_clusters': '5'
+}
+init_path = utils.config_training(universal, parameters, hyperparameters)
+config_GMM = utils.Configuration(init_path)
+config_GMM.load_config()
+config_GMM.set_device(device_no)
+config_GMM.show = True
+train(config_GMM)
