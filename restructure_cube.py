a63249a801543c7269ea5826ac3b90919c8a4c39
Julia310 <JPeters92@protonmail.com>
2023-12-21 16:26:25 +0100

    Compute the chunk to trigger data loading and perform operations

--- restructure_cube.py
+++ restructure_cube.py
@@ -26,9 +26,12 @@ time_chunks = get_chunk_boundaries(ds.dims['time'], 60)
 channel_chunks = get_chunk_boundaries(ds.dims['channel'], 5)
 freq_chunks = get_chunk_boundaries(ds.dims['freq'], 101)  # Assuming one chunk for 'freq'
 
+
 # Iterate over chunks
 for t_start, t_end in time_chunks:
     for c_start, c_end in channel_chunks:
         for f_start, f_end in freq_chunks:
             chunk = ds.isel(time=slice(t_start, t_end), channel=slice(c_start, c_end), freq=slice(f_start, f_end))
-            print(chunk)
+            # Compute the chunk to trigger data loading and perform operations
+            computed_chunk = chunk.compute()
+            print(computed_chunk)
