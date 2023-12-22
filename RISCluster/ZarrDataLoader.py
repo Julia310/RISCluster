4cff26ca8b62e9f3c999c18bb6c16070ac0bbd34
Julia310 <JPeters92@protonmail.com>
2023-12-22 18:35:20 +0100

    you dask to load data

--- RISCluster/ZarrDataLoader.py
+++ RISCluster/ZarrDataLoader.py
@@ -1,13 +1,12 @@
 import torch
 import xarray as xr
 import zarr
-from torch.utils.data import DataLoader
 from torchvision import transforms
-from torch.utils.data import random_split, Subset
+from torch.utils.data import random_split
 from torch.utils.data import Dataset
-import os
-from zarr.storage import KVStore
 import numpy as np
+import dask.array as da
+
 
 
 
@@ -35,34 +34,70 @@ class ZarrDataset(Dataset):
 
     class SpecgramToTensor(object):
         def __call__(self, X):
+            #print('initial shape: ', X.shape)
             X = np.expand_dims(X, axis=0)
             return torch.from_numpy(X)
 
+    def __init__(self, zarr_path, sample_size, transform=None, chunk_size=2879 * 2):
+        # Open the Zarr array with Zarr and wrap it with Dask
+        zarr_array = zarr.open(zarr_path, mode='r')
+        dask_array = da.from_zarr(zarr_array)
 
-    def __init__(self, zarr_path, chunk_size, transform=None):
-        self.zarr_array = zarr.open(zarr_path, mode='r')
-        self.chunk_size = chunk_size  # Size of each chunk in the first dimension
+        # Convert the Dask-backed array to an Xarray DataArray, applying chunking
+        self.ds = xr.DataArray(dask_array, dims=['time', 'channel', 'freq']).chunk(
+            {'time': chunk_size, 'channel': 1, 'freq': 101})
+
+        self.chunk_size_time = chunk_size  # Total size of each chunk in the 'time' dimension
+        self.chunk_size_channel = 1  # Total size of each chunk in the 'channel' dimension
+        self.sample_size = sample_size  # Size of each individual sample in the 'time' dimension
+        self.sample_shape = (sample_size, 1, 101)
         self.transform = transform
-        #self.num_sequences = self.zarr_array.shape[1]  # Number of sequences in the second dimension
-        self.num_sequences = 50  # Number of sequences in the second dimension
-        #self.samples_per_sequence = self.zarr_array.shape[0] // chunk_size  * 2# Number of chunks per sequence
-        self.samples_per_sequence = self.zarr_array.shape[0] * 2 // (11 * 24 * chunk_size)# Number of chunks per sequence
-        print('samples per sequence', self.samples_per_sequence)
-        print('number of sequences', self.num_sequences)
+
+        self.num_chunks = (self.ds.shape[0] // chunk_size) * self.ds.shape[1]
+        self.samples_per_chunk = (self.chunk_size_time // self.sample_size * 2 - 1) * self.chunk_size_channel  # Number of samples per chunk, considering overlap
+        self.total_samples = self.samples_per_chunk * self.num_chunks
+
+        # Initialize variables to manage chunk loading
+        self.preloaded_chunk = None
+        self.current_chunk_index = -1
+        self.current_channel = 0
 
     def __len__(self):
-        return self.samples_per_sequence * self.num_sequences
+        return self.total_samples
+
+    def load_chunk(self, chunk_index):
+        start_idx_time = chunk_index * self.chunk_size_time
+        end_idx_time = start_idx_time + self.chunk_size_time
+
+        start_idx_channel = self.current_channel
+        end_idx_channel = self.current_channel + 1
+
+        # Extract the larger chunk from the dataset
+        self.preloaded_chunk = self.ds.isel(
+            time=slice(start_idx_time, end_idx_time),
+            channel=slice(start_idx_channel, end_idx_channel)
+        ).compute()
+        self.current_chunk_index = chunk_index
+
+        if end_idx_time % self.ds.shape[0] == 0 and end_idx_time != 0:
+            self.current_channel += 1
 
     def __getitem__(self, idx):
-        sequence_idx = idx // self.samples_per_sequence
-        chunk_idx = idx % self.samples_per_sequence
+        sequence_idx = idx // self.total_samples
+        sample_idx_within_sequence = idx % self.total_samples
+        chunk_index = (sample_idx_within_sequence // self.samples_per_chunk) % (self.ds.shape[0] // self.chunk_size_time)
+        sample_idx_in_chunk = sample_idx_within_sequence % self.samples_per_chunk
 
-        # Calculate the start and end indices for the chunk
-        start_idx = chunk_idx * self.chunk_size // 2
-        end_idx = start_idx + self.chunk_size
+        # Load the chunk if it's not already loaded
+        if chunk_index != self.current_chunk_index:
+            self.load_chunk(chunk_index)
 
-        # Extract the chunk from the Zarr array
-        sample = self.zarr_array[start_idx:end_idx, sequence_idx, :].astype(np.float64)
+        # Calculate the indices to extract the small sample from the preloaded chunk
+        sample_start = sample_idx_in_chunk * self.sample_shape[0] // 2
+        sample_end = sample_start + self.sample_shape[0]
+
+        # Extract the small sample
+        sample = self.preloaded_chunk[sample_start:sample_end, sequence_idx, :].astype(np.float64)
 
         if self.transform:
             sample = self.transform(sample)
@@ -78,9 +113,9 @@ def get_zarr_data(split_dataset=True):
         lambda x: x.double(),
     ])
 
-    chunk_size = 4
-    #full_dataset = ZarrDataset('./1907_NEW_1Hz_TRUNC.zarr', chunk_size, transform=transform_pipeline)
-    full_dataset = ZarrDataset('/work/users/jp348bcyy/rhoneCubeNeu/Cube.zarr', chunk_size, transform=transform_pipeline)
+    sample_size = 4
+    #full_dataset = ZarrDataset('./1907_NEW_1Hz_TRUNC.zarr', sample_size, transform=transform_pipeline)
+    full_dataset = ZarrDataset('/work/users/jp348bcyy/rhoneCubeNeu/Cube.zarr', sample_size, transform=transform_pipeline)
     print('full dataset length: ', len(full_dataset))
 
     if split_dataset:
@@ -98,28 +133,3 @@ def get_zarr_data(split_dataset=True):
         return full_dataset
 
 
-
-#dataset = ZarrDataset('1907_NEW_1Hz_TRUNC.zarr', transform=transform_to_tensor)
-#dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-# Example of iterating over batches
-#for batch in dataloader:
-#        print(batch.shape)
-
-
-#train_dataloader, test_dataloader = get_zarr_data(batch_size=32, workers=5)
-
-
-
-# Training loop
-#for batch in train_dataloader:
-#    # Your training code here
-#    print("traindata")
-#    print(batch.shape)
-
-
-# Evaluation loop
-#for batch in test_dataloader:
-#    # Your evaluation code here
-#    print("testdata")
-#    print(batch.shape)
