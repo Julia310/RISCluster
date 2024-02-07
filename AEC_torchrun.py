d38aae4c98bed19e17690e7d97f0a882b10585a7
Julia310 <JPeters92@protonmail.com>
2024-02-07 22:33:33 +0100

    add print statement for process validation

--- AEC_torchrun.py
+++ AEC_torchrun.py
@@ -57,7 +57,7 @@ class Trainer:
 
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
-        #print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
         self.train_data.sampler.set_epoch(epoch)
         for batch in self.train_data:
             batch_size, mini_batch, channels, height, width = batch.size()
