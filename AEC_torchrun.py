f1041a5edc015aecc7b5dd2b944b5753305cfd7f
Julia310 <JPeters92@protonmail.com>
2024-02-10 15:47:54 +0100

    adjust batch_size, num_workers

--- AEC_torchrun.py
+++ AEC_torchrun.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
-    parser.add_argument('--batch_size', default=8, type=int, help='Input batch size on each device (default: 32)')
+    parser.add_argument('--batch_size', default=5, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
 
     main(args.save_every, args.total_epochs, args.batch_size)
